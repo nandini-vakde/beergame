ba29b5e9645750ab41fa754bcc33c9cfff3b0e22
nandini-vakde <nvakde@purdue.edu>
2024-10-29 22:12:25 -0400

    Update beergame.py

--- beergame.py
+++ beergame.py
@@ -22,10 +22,18 @@ class Subsession(BaseSubsession):
                 random.shuffle(roles)
                 for player, role in zip(players, roles):
                     player.game_role = role
+
+            # should allow access to specific groups, returns group matrix
+            matrix = subsession.get_group_matrix()
+            for row in matrix:
+                row.reverse()  
+                subsession.set_group_matrix(matrix)
         else:
             subsession.group_like_round(1)
+    
 
 class Group(BaseGroup):
+    # arrays to keep track of inventories and order amounts across rounds
     brewery_inventories = models.LongStringField(initial='[]')
     distributor_inventories = models.LongStringField(initial='[]')
     wholesaler_inventories = models.LongStringField(initial='[]')
@@ -36,6 +44,7 @@ class Group(BaseGroup):
     wholesaler_order_amounts = models.LongStringField(initial='[]')
     retailer_order_amounts = models.LongStringField(initial='[]')
 
+    # functions to update arrays
     def update_order_amounts(self):
         for player in self.get_players():
             if player.game_role == 'Brewery':
@@ -77,23 +86,27 @@ class Player(BasePlayer):
     cost = models.CurrencyField(initial=0)
 
     def calculate_inventory(self):
-        # still need to calculate in transit, demand, and backorder
+        # still need to calculate in transit, demand
         for player in self.get_players():
             if player.game_role == 'Brewery':
                 if len(distributor_order_amounts) >= self.group.subsession.round_number - 1:
-                    self.inventory = max(0, self.inventory - distributor_order_amounts[self.group.subsession.round_number - 2])
+                    self.inventory = max(0, self.inventory - distributor_order_amounts[self.group.subsession.round_number - 3] + brewery_order_amounts[self.group.subsession.round_number - 2])
+                    self.backorder = self.backorder + (self.inventory - distributor.order_amount)
+                    
             elif player.game_role == 'Distributor':
                 if len(wholesaler_order_amounts) >= self.group.subsession.round_number - 1:
-                    self.inventory = max(0, self.inventory - wholesaler_order_amounts[self.group.subsession.round_number - 2])
+                    self.inventory = max(0, self.inventory - wholesaler_order_amounts[self.group.subsession.round_number - 3] + distributor_order_amounts[self.group.subsession.round_number - 3])
+                    self.backorder = self.backorder + (self.inventory - wholesaler.order_amount)
             elif player.game_role == 'Wholesaler':
                 if len(retailer_order_amounts) >= self.group.subsession.round_number - 1:
-                    self.inventory = max(0, self.inventory - retailer_order_amounts[self.group.subsession.round_number - 2])
+                    self.inventory = max(0, self.inventory - retailer_order_amounts[self.group.subsession.round_number - 3] + wholesaler_order_amounts[self.group.subsession.round_number - 3])
+                    self.backorder = self.backorder + (self.inventory - retailer.order_amount)
+                    
             elif player.game_role == 'Retailer':
-                retailer_order_amounts.append(player.order_amounts)
-        # Simplified inventory logic
-        self.inventory = max(0, self.inventory - self.order_amount) # Adjust for real logic
-        self.backorder = max(0, self.order_amount - self.inventory) # Adjust for real logic
-
+                # if len(???) >= self.group.subsession.round_number - 1:
+                    # self.inventory = max(0, self.inventory - ???[self.group.subsession.round_number - 3] + retailer_order_amounts[self.group.subsession.round_number - 3])
+                    # self.backorder = self.backorder + (self.inventory - ???.order_amount)
+        
 
 class Introduction(Page):
     def vars_for_template(self):
@@ -109,6 +122,7 @@ class OrderPage(Page):
         for player in self.get_players():
             if isinstance(self.player.order_amount, int) and 0 < self.order_amount < 10000:
                 self.group.update_order_amounts()
+                self.group.update_inventories()
                 self.player.calculate_inventory()
             # else ask again?
 
